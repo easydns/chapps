08812782bd2c6db47b42b3aee34bbf53af20d012
Caleb Cullen <39280690+caleb-s-cullen@users.noreply.github.com>
2022-02-28 16:40:30 -0500

    moved some settings to setup.cfg, and adding a bunch of code to setup.py in order to perform some post-install tasks: mainly to create systemD service files for the newly installed software, with sensitivity to whether the code will run from a venv

--- setup.py
+++ setup.py
@@ -1,5 +1,91 @@
 from setuptools import setup
+from setuptools.command.install import install
+from pathlib import Path
+import jinja2
 import re
+import sys
+import atexit
+import dbus
+
+SERVICES = [
+    'services/chapps_outbound_quota.py',
+    'services/chapps_outbound_multi.py',
+    'services/chapps_greylisting.py',
+]
+
+SERVICE_DESCRIPTIONS = {
+    'services/chapps_outbound_quota.py': 'CHAPPS Outbound Quota Service',
+    'services/chapps_outbound_multi.py': 'CHAPPS Outbound Multipolicy Service',
+    'services/chapps_greylisting.py': 'CHAPPS Greylisting Service',
+}
+
+class PostInstallSetup(install):
+    def get_base_prefix_compat( self ):
+        return getattr( sys, 'base_prefix', None ) or getattr( sys, 'real_prefix', None ) or sys.prefix
+
+    def in_virtualenv( self ):
+        return get_base_prefix_compat() != sys.prefix
+
+    def post_install_setup_factory( self ):
+        """This function returns a closure suitable for performing the post-install setup"""
+        env_settings = {}
+        template_locations = []
+        template_found = []
+        service_descriptions = SERVICE_DESCRIPTIONS
+        if self.in_virtualenv():
+            template_loc = Path( sys.prefix ) / "chapps" / "install"
+            template_locations.append( template_loc ) if template_loc.is_dir()
+            service_bin = Path( sys.prefix ) / "bin"
+            python_invocation = str( service_bin / 'python3' )
+            env_settings['CHAPPS_CONFIG'] = str( Path( sys.prefix ) / "etc" )
+        else:
+            template_loc = Path( sys.prefix ) / "chapps" / "install"
+            service_bin = Path( sys.prefix )  / "bin"
+            if ( Path( sys.prefix ) / "local" ).is_dir():
+                possible_template_loc = Path( sys.prefix ) / "local" / "chapps" / "install"
+                if possible_template_loc.is_dir():
+                    template_loc = possible_template_loc
+                    template_locations.append( template_loc )
+                    service_bin = Path( sys.prefix ) / "local" / "bin"
+            template_locations.append( Path( sys.prefix ) / "chapps" / "install" )
+            python_invocation = '/usr/bin/env python3' )
+        def template_reader( template_file ):
+            """This function finds and returns the contents of a template"""
+            for template_dir in template_locations:
+                template_path = template_dir / str( template_file )
+                try:
+                    with template_path.open() as tfh:
+                        template_found[0] = template_path
+                        return tfh.read_text()
+                except FileNotFoundError:
+                    next
+            return ""
+        def post_install_setup():
+            """This function performs the setup steps: template out service files, then connect them to SystemD"""
+            templater = jinja2.Environment(
+                loader=jinja2.FunctionLoader( template_reader ),
+                autoescape=jinja2.select_autoescape()
+            )
+            service_template = templater.get_template( 'chapps-systemd-service.tmpl' )
+            services_to_enable = []
+            for service_source, service_desc in service_descriptions.items():
+                service_exec_path = service_bin / Path( service_source ).name
+                service_file_path = template_found[0].parent / ( Path( service.source ).stem + '.service' )
+                with service_file_path.open( 'w' ) as outfile:
+                    outfile.write( service_template.render( **locals ) )
+                services_to_enable.append( service_file_path )
+            sysbus = dbus.SystemBus()
+            systemd = sysbus.get_object( 'org.freedesktop.systemd1', '/org/freedesktop/systemd1' )
+            manager = dbus.Interface( systemd, 'org.freedesktop.systemd1.Manager' )
+            manager.LinkUnitFiles( services_to_enable, False, True )
+            manager.Reload()
+
+    def __init__( self, *args, **kwargs ):
+        super().__init__( *args, **kwargs )
+        atexit.register( self.post_install_setup_factory() )
+
+
+
 VERSIONFILE = 'chapps/_version.py'
 verstrline = open(VERSIONFILE, 'rt').read()
 VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
@@ -10,9 +96,13 @@ else:
     raise RuntimeError( f"Unable to find version string in {VERSIONFILE}" )
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+
 setup(
+  cmdclass={ 'install': PostInstallSetup }
   name = 'chapps',
   packages = ['chapps'],
+  package_data = { 'chapps' : }
   data_files = [
       ('chapps', [ 'README.md',
              'INSTALLATION.md',]),
@@ -28,14 +118,11 @@ setup(
                                   'postfix/null_filter/master.cf']),
   ],
   scripts = [
-      'services/chapps_outbound_quota.py',
-      'services/chapps_outbound_multi.py',
-      'services/chapps_greylisting.py',
+      *SERVICES,
       'services/null_filter.py',
       'install/chapps_database_init.py'
   ],
   version = verstr,
-  python_requires='>=3.8',
   license='MIT',
   description = 'Caching, Highly-Available Postfix Policy Service',
   long_description=long_description,
@@ -45,23 +132,12 @@ setup(
   url = 'https://gitlab.int.easydns.net/ccullen/chapps',
   download_url = f'https://github.com/easydns/chapps/tree/main/dist/chapps-{verstr}.tar.gz',
   keywords = ['Postfix', 'Policy', 'Daemon'],
-  install_requires=[
-          'redis',
-          'expiring-dict',
-          'dnspython',
-          'mariadb',
-          'pyspf',
-          'python-pidfile',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Environment :: No Input/Output (Daemon)',
-    'Intended Audience :: System Administrators',
-    'Operating System :: POSIX :: Linux',
-    'Topic :: Communications :: Email :: Filters',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
-    'Programming Language :: Python :: 3.10',
-  ],
+  # install_requires=[       # dependencies come from setup.cfg now
+  #         'redis',
+  #         'expiring-dict',
+  #         'dnspython',
+  #         'mariadb',
+  #         'pyspf',
+  #         'python-pidfile',
+  #     ],
 )
