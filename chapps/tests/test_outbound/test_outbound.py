6b53ca66a62f044b317610b408283eef39f4ffa4
Caleb Cullen <39280690+caleb-s-cullen@users.noreply.github.com>
2022-03-17 18:24:15 -0400

    adding test framework to test new logic which recognizes auth failure when the user-key (sasl_username) is not present; I am not sure of the behavior when ccert auth is being used, and fails

--- chapps/tests/test_outbound/test_outbound.py
+++ chapps/tests/test_outbound/test_outbound.py
@@ -1,6 +1,8 @@
 import pytest
 import logging
 from chapps.outbound import OutboundPPR
+from chapps.signals import AuthenticationFailureException
+from chapps.config import CHAPPSConfig
 
 
 class Test_OutboundPPR:
@@ -14,35 +16,45 @@ class Test_OutboundPPR:
         assert testing_userppr.user is not None
         assert testing_userppr.user != "None"
 
-    def test_user_default_no_sasl(self, monkeypatch, chapps_test_env, testing_userppr):
+    def test_no_user_key_no_auth(self, monkeypatch, chapps_test_env, testing_userppr):
         with monkeypatch.context() as m:
             m.setattr(testing_userppr, "sasl_username", None)
             assert type(testing_userppr) == OutboundPPR
-            assert testing_userppr.user == testing_userppr.ccert_subject
-            assert testing_userppr.user is not None
-            assert testing_userppr.user != "None"
+            with pytest.raises(AuthenticationFailureException):
+                assert testing_userppr.user
+
+    def test_user_default_no_sasl(self, monkeypatch, chapps_mock_env, chapps_mock_cfg_path, mocking_userppr):
+        assert chapps_mock_env == chapps_mock_cfg_path
+        assert str(CHAPPSConfig.what_config_file()) == chapps_mock_cfg_path
+        assert not mocking_userppr._config.require_user_key
+        with monkeypatch.context() as m:
+            m.setattr(mocking_userppr, "sasl_username", None)
+            assert type(mocking_userppr) == OutboundPPR
+            assert mocking_userppr.user == mocking_userppr.ccert_subject
+            assert mocking_userppr.user is not None
+            assert mocking_userppr.user != "None"
 
     def test_user_default_no_sasl_or_ccert(
-        self, monkeypatch, chapps_test_env, testing_userppr
+        self, monkeypatch, chapps_mock_env, mocking_userppr
     ):
         with monkeypatch.context() as m:
-            m.setattr(testing_userppr, "sasl_username", None)
-            m.setattr(testing_userppr, "ccert_subject", None)
-            assert type(testing_userppr) == OutboundPPR
-            assert testing_userppr.user == testing_userppr.sender
-            assert testing_userppr.user is not None
-            assert testing_userppr.user != "None"
+            m.setattr(mocking_userppr, "sasl_username", None)
+            m.setattr(mocking_userppr, "ccert_subject", None)
+            assert type(mocking_userppr) == OutboundPPR
+            assert mocking_userppr.user == mocking_userppr.sender
+            assert mocking_userppr.user is not None
+            assert mocking_userppr.user != "None"
 
     def test_user_default_no_sasl_ccert_or_sender(
-        self, caplog, monkeypatch, chapps_test_env, testing_userppr
+        self, caplog, monkeypatch, chapps_mock_env, mocking_userppr
     ):
         caplog.set_level(logging.DEBUG)
         with monkeypatch.context() as m:
-            m.setattr(testing_userppr, "sasl_username", None)
-            m.setattr(testing_userppr, "ccert_subject", None)
-            m.setattr(testing_userppr, "sender", None)
-            assert type(testing_userppr) == OutboundPPR
-            assert testing_userppr.user is not None
-            assert testing_userppr.user != "None"
-            assert testing_userppr.user == testing_userppr.client_address
-            assert testing_userppr.user == "10.10.10.10"
+            m.setattr(mocking_userppr, "sasl_username", None)
+            m.setattr(mocking_userppr, "ccert_subject", None)
+            m.setattr(mocking_userppr, "sender", None)
+            assert type(mocking_userppr) == OutboundPPR
+            assert mocking_userppr.user is not None
+            assert mocking_userppr.user != "None"
+            assert mocking_userppr.user == mocking_userppr.client_address
+            assert mocking_userppr.user == "10.10.10.10"
