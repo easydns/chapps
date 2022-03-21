34b26ba26be76ef8339dfc847b1e48e4fc9d3d8c
Caleb Cullen <39280690+caleb-s-cullen@users.noreply.github.com>
2022-03-21 18:48:20 -0400

    adding test for quota creation via auto-generated route
    also adjusting some older tests to accommodate the database
    refreshment -- previous tests reflected some non-standard entries not
    reflected in the fixtures which build the test database

--- chapps/tests/test_api/test_api.py
+++ chapps/tests/test_api/test_api.py
@@ -6,14 +6,13 @@ import chapps.config
 class Test_Users_API:
     """Tests of the User CRUD API"""
 
-    def test_get_user(self, fixed_time, testing_api_client):
+    def test_get_user(
+        self, fixed_time, testing_api_client, populated_database_fixture
+    ):
         response = testing_api_client.get("/users/1")
         assert response.status_code == 200
         assert response.json() == {
-            "domains": [
-                {"id": 1, "name": "chapps.io"},
-                {"id": 2, "name": "easydns.com"},
-            ],
+            "domains": [{"id": 1, "name": "chapps.io"}],
             "quota": {"id": 1, "name": "10eph", "quota": 240},
             "response": {"id": 1, "name": "ccullen@easydns.com"},
             "timestamp": fixed_time,
@@ -55,10 +54,7 @@ class Test_Domains_API:
         response = testing_api_client.get("/domains/")
         assert response.status_code == 200
         assert response.json() == {
-            "response": [
-                {"id": 1, "name": "chapps.io"},
-                {"id": 2, "name": "easydns.com"},
-            ],
+            "response": [{"id": 1, "name": "chapps.io"}],
             "timestamp": fixed_time,
             "version": "CHAPPS v0.4",
         }
@@ -85,6 +81,19 @@ class Test_Quotas_API:
                 {"id": 2, "name": "50eph", "quota": 1200},
                 {"id": 3, "name": "200eph", "quota": 4800},
             ],
+            "timestamp": fixed_time,
+            "version": "CHAPPS v0.4",
+        }
+
+    def test_create_quota(
+        self, fixed_time, testing_api_client, populated_database_fixture
+    ):
+        response = testing_api_client.post(
+            "/quotas/", json={"name": "400eph", "quota": 9600}
+        )
+        assert response.status_code == 201
+        assert response.json() == {
+            "response": {"id": 4, "name": "400eph", "quota": 9600},
             "timestamp": fixed_time,
             "version": "CHAPPS v0.4",
         }
