6f25b7000127680c96120ba2d0e5752be171688f
Caleb Cullen <39280690+caleb-s-cullen@users.noreply.github.com>
2022-03-21 18:47:05 -0400

    adding imports for database refreshment, needed for testing creation/deletion, etc.

--- chapps/tests/test_api/conftest.py
+++ chapps/tests/test_api/conftest.py
@@ -7,23 +7,33 @@ from chapps.tests.test_config.conftest import (
     chapps_mock_config_file,
     chapps_mock_env,
 )
+from chapps.tests.test_adapter.conftest import (
+    finalizing_pcadapter,
+    base_adapter_fixture,
+    database_fixture,
+    populated_database_fixture,
+)
 import chapps.config
 import time
+
 
 def pytest_configure():
     pytest.FIXED_TIME = 1647625876.036144
 
+
 @pytest.fixture
 def fixed_time(monkeypatch):
     with monkeypatch.context() as m:
-        m.setattr(time, 'time', Mock(return_value=pytest.FIXED_TIME))
+        m.setattr(time, "time", Mock(return_value=pytest.FIXED_TIME))
         yield pytest.FIXED_TIME
+
 
 @pytest.fixture
 def testing_api_client(monkeypatch, chapps_mock_env, chapps_mock_config_file):
     cfg = chapps.config.CHAPPSConfig()
     with monkeypatch.context() as m:
-        m.setattr(chapps.config, 'config', cfg)
+        m.setattr(chapps.config, "config", cfg)
         from chapps.rest.api import api
+
         client = TestClient(api)
         yield client
