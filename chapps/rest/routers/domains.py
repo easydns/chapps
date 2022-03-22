90755987d7618b7d20abc23983f5a723f1ae69b1
Caleb Cullen <39280690+caleb-s-cullen@users.noreply.github.com>
2022-03-22 17:55:50 -0400

    updates to domain to add CREATE route, and logic to begin testing it

--- chapps/rest/routers/domains.py
+++ chapps/rest/routers/domains.py
@@ -1,4 +1,5 @@
 from typing import List
+from starlette import status
 from fastapi import APIRouter  # , Body, Path, HTTPException
 from chapps.util import AttrDict
 from chapps.rest.models import User, Domain, DomainResp, DomainsResp
@@ -12,7 +13,9 @@ logger.setLevel(chapps.logging.DEFAULT_LEVEL)
 api = APIRouter(
     prefix="/domains",
     tags=["domains"],
-    responses={404: {"description": "Domain not found."}},
+    responses={
+        status.HTTP_404_NOT_FOUND: {"description": "Domain not found."}
+    },
 )
 
 api.get("/")(list_items(Domain, response_model=DomainsResp))
@@ -21,7 +24,15 @@ api.get("/{item_id}")(
     get_item_by_id(Domain, response_model=DomainResp, assoc=[(User, "users")])
 )
 
-api.post("/")(
+api.post(
+    "/",
+    status_code=status.HTTP_201_CREATED,
+    responses={
+        status.HTTP_409_CONFLICT: {
+            "description": "Database integrity conflict."
+        }
+    },
+)(
     create_item(
         Domain,  # default params argument will suffice
         response_model=DomainResp,
@@ -30,7 +41,9 @@ api.post("/")(
                 model=User,
                 name="users",
                 type_=List[int],
-                join_table=User.metadata.tables["domain_user"],
+                join_table=Domain.metadata.tables["domain_user"],
+                source_id="domain_id",
+                model_id="user_id",
             )
         ],
     )
--- chapps/tests/test_api/test_api.py
+++ chapps/tests/test_api/test_api.py
@@ -59,6 +59,19 @@ class Test_Domains_API:
             "version": "CHAPPS v0.4",
         }
 
+    def test_create_domain(
+        self, fixed_time, testing_api_client, populated_database_fixture
+    ):
+        response = testing_api_client.post(
+            "/domains/", json={"name": "easydns.com"}
+        )
+        assert response.status_code == 201
+        assert response.json() == {
+            "response": {"id": 2, "name": "easydns.com"},
+            "timestamp": fixed_time,
+            "version": "CHAPPS v0.4",
+        }
+
 
 class Test_Quotas_API:
     """Tests of the Quota CRUD API"""
