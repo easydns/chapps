e508d521802bf69a29c8e7dcaaa745d45a15de1e
Caleb Cullen <39280690+caleb-s-cullen@users.noreply.github.com>
2022-03-23 15:37:14 -0400

    some changes to logging, to send some message to the mail log
    This may result in double-logging of a sort which cannot easily be
    remedied by rsyslog configuration.  However, in practice, one might
    set the DEFAULT (syslog) loglevel to WARNING or higher, in order to
    see only messages about emergent situations, while the mail log set to
    INFO will collect more data.  Ideally, warning and error messages
    are rare.

--- chapps/logging.py
+++ chapps/logging.py
@@ -2,19 +2,28 @@
 import logging
 from logging.handlers import SysLogHandler
 
+DEFAULT_LEVEL = logging.DEBUG
+MAILLOG_LEVEL = logging.INFO
+
 
 class LogSetup:  # pragma: no cover
-    ROOT_LOG_FORMAT = "CHAPPS:%(levelname)s %(filename)s@%(lineno)s: %(message)s"
-    MAIN_LOG_FORMAT = "CHAPPS:%(levelname)s %(name)s@%(funcName)s: %(message)s"
+    syslog_formatter = logging.Formatter("CHAPPS:%(levelname)s %(filename)s@%(lineno)s: %(message)s")
+    maillog_formatter = logging.Formatter("CHAPPS:%(levelname)s %(message)s")
+
+    syslog_handler = SysLogHandler(facility=SysLogHandler.LOG_LOCAL0, address="/dev/log")
+    syslog_handler.setLevel(DEFAULT_LEVEL)
+    syslog_handler.setFormatter(syslog_formatter)
+
+    maillog_handler = SysLogHandler(facility=SysLogHandler.LOG_MAIL, address="/dev/log")
+    maillog_handler.setLevel(MAILLOG_LEVEL)
+    maillog_handler.setFormatter(maillog_formatter)
 
     def __init__(self):
         if not logging.getLogger(None).hasHandlers():
             logging.basicConfig(
-                format=self.ROOT_LOG_FORMAT,
-                handlers=[
-                    SysLogHandler(facility=SysLogHandler.LOG_LOCAL0, address="/dev/log")
-                ],
+                handlers=[debug_handler, maillog_handler]
             )
 
 
 logsetup = LogSetup()  # pragma: no cover
+logger = logging.getLogger("chapps")
