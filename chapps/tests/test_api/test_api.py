bbb8e3b5a4c4f69923dcd6547f76c1593dbffa66
Caleb Cullen <39280690+caleb-s-cullen@users.noreply.github.com>
2022-03-25 17:08:51 -0400

    unfinished test for deleting items
    for one thing, I want to look at the swagger docs
    and also, I want to see what the library does when an attempt is made
    to delete a nonexistent record

--- chapps/tests/test_api/test_api.py
+++ chapps/tests/test_api/test_api.py
@@ -125,6 +125,18 @@ class Test_Domains_API:
             "version": "CHAPPS v0.4",
         }
 
+    @pytest.mark.timeout(2)
+    def test_delete_domain(
+        self, fixed_time, testing_api_client, populated_database_fixture
+    ):
+        response = testing_api_client.delete("/domains/", json={1})
+        assert response.status_code == 200
+        assert response.json() == {
+            "status": True,
+            "timestamp": fixed_time,
+            "version": "CHAPPS v0.4",
+        }
+
 
 class Test_Quotas_API:
     """Tests of the Quota CRUD API"""
