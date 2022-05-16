06c1182f3fc11bdf7684b11cbb097f9f001609b7
Caleb Cullen <39280690+caleb-s-cullen@users.noreply.github.com>
2022-05-16 13:33:47 -0400

    Adding references to README at the project page
    and also to INSTALLATION; at the same time removing them from the
    RTD docs, for now

--- chapps/__init__.py
+++ chapps/__init__.py
@@ -1,6 +1,11 @@
 """CHAPPS
 
 Caching, Highly-Available Postfix Policy Service
+
+In addition to module documentation found here, an introduction and overview of
+the project is presented in its README_.  Specific concerns and instructions
+about getting it running are discussed in the INSTALLATION_ file.
+
 """
 from ._version import __version__
 import chapps.logging
