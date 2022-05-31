b538fdeaba522c6ba43d0411a7d139304a985f74
Caleb Cullen <39280690+caleb-s-cullen@users.noreply.github.com>
2022-05-31 09:58:36 -0400

    fixing a bug in the fix

--- chapps/util.py
+++ chapps/util.py
@@ -414,8 +414,9 @@ class PostfixPolicyRequest(Mapping):
             (l for l in self._payload if attr == l.split("=")[0]), None
         )
         if line:
-            key, *value = line.split("=")
-            setattr(self, key, "=".join(value))
+            key, *values = line.split("=")
+            value = "=".join(values)
+            setattr(self, key, value)
             return value
         else:
             logger.debug(f"No lines in {self} matched {attr}.")
