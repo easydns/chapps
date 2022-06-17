bb1f412ebaab22ffaea198aff337c2eb9957e76d
Caleb Cullen <39280690+caleb-s-cullen@users.noreply.github.com>
2022-06-17 13:22:46 -0400

    added CHAPPS imports to alembin/env.py for DB access
    also modified the tops of the routines to obtain URL and Engine properly.

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,12 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from chapps.dbsession import (
+    sql_engine as chapps_engine,
+    create_db_url as chapps_db_url,
+)
+from chapps.dbmodels import DB_Base
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -18,7 +24,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = DB_Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -38,7 +44,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = chapps_db_url()
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -57,11 +63,7 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    connectable = chapps_engine
 
     with connectable.connect() as connection:
         context.configure(
