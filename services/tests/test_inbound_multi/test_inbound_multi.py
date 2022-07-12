5f1fd0e1f2b59f4b064a12788b1efd80713568c1
Caleb Cullen <39280690+caleb-s-cullen@users.noreply.github.com>
2022-07-12 10:33:34 -0400

    tests for inbound multipolicy service
    these tests cover all four different possible scenarios, and
    demonstrate that the service can properly communicate with Postfix
    while enforcing SPF and Greylisting simultaneously.

--- services/tests/test_inbound_multi/test_inbound_multi.py
+++ services/tests/test_inbound_multi/test_inbound_multi.py
@@ -28,7 +28,9 @@ class Test_IBM_Greylisting:
         caplog.set_level(logging.DEBUG)
         message = grl_test_message_factory(known_sender, grl_test_recipients)
         with SMTP("127.0.0.1") as smtp:
-            with pytest.raises(SMTPRecipientsRefused):
+            with pytest.raises(
+                SMTPRecipientsRefused, match="temporarily stupid"
+            ):
                 assert smtp.sendmail(
                     known_sender, grl_test_recipients, message
                 )
@@ -39,7 +41,7 @@ class Test_IBM_Greylisting:
         chapps_ibm_service_with_tuples_factory,
         known_sender,
         grl_test_recipients,
-        grl_test_message_factory,
+        ibm_test_message_factory,
     ):
         """
         GIVEN an email is being retried after deferral (the tuple has been seen)
@@ -47,14 +49,10 @@ class Test_IBM_Greylisting:
         THEN  it should be accepted (using DUNNO to allow other filters)
         """
         caplog.set_level(logging.DEBUG)
-        with chapps_ibm_service_with_tuples_factory([grl_test_recipients]):
-            message = grl_test_message_factory(
-                known_sender, grl_test_recipients
-            )
-            with SMTP("127.0.0.1") as smtp:
-                result = smtp.sendmail(
-                    known_sender, grl_test_recipients, message
-                )
+        message = ibm_test_message_factory(known_sender, grl_test_recipients)
+        chapps_ibm_service_with_tuples_factory([grl_test_recipients])
+        with SMTP("127.0.0.1") as smtp:
+            result = smtp.sendmail(known_sender, grl_test_recipients, message)
         assert True  # email was accepted
 
     def test_accept_emails_from_proven_clients(
@@ -125,4 +123,122 @@ class Test_IBM_SPF:
         with chapps_ibm_service_with_tally_factory([(sender, recip)]):
             with SMTP("127.0.0.1") as smtp:
                 result = smtp.sendmail(sender, recip, message)
+        assert True
+
+    def test_enforcement_skipped(
+        self,
+        caplog,
+        chapps_ibm_service,
+        known_sender,
+        no_enforcement_recipients,
+        ibm_test_message_factory,
+    ):
+        caplog.set_level(logging.DEBUG)
+        recip = no_enforcement_recipients
+        sender = known_sender
+        message = ibm_test_message_factory(sender, recip)
+        with SMTP("127.0.0.1") as smtp:
+            result = smtp.sendmail(sender, recip, message)
+        assert True  # note that SPF should fail this sender
+
+
+class Test_IBM_SPF_and_Greylisting:
+    """Test interaction of both
+
+    In cases where both are enabled for a domain, messages passing SPF are also
+    greylisted autonomously by the GreylistingPolicy rather than as a special
+    request on behalf of the SPF policy, which happens for soft fails.
+    """
+
+    def test_passing_first_attempt_deferred(
+        self,
+        caplog,
+        clear_redis_grl,
+        chapps_ibm_service,  # no redis setup
+        spf_and_grl_recipients,
+        passing_spf_sender,
+        ibm_test_message_factory,
+    ):
+        """
+        :GIVEN: a new email delivery attempt, for an unrecognized tuple
+        :WHEN:  presented for delivery with SPF and Greylisting enforcement
+        :AND:   SPF passes
+        :THEN:  it should be deferred
+        """
+        caplog.set_level(logging.DEBUG)
+        sender = passing_spf_sender
+        recip = spf_and_grl_recipients
+        message = ibm_test_message_factory(sender, recip)
+        with SMTP("127.0.0.1") as smtp:
+            with pytest.raises(SMTPRecipientsRefused, match="4.7.1"):
+                result = smtp.sendmail(sender, recip, message)
+
+    def test_accept_after_deferral(
+        self,
+        caplog,
+        clear_redis_grl,
+        chapps_ibm_service_with_tuples_factory,
+        passing_spf_sender,
+        spf_and_grl_recipients,
+        ibm_test_message_factory,
+    ):
+        """
+        :GIVEN: an email is being retried after deferral (the tuple has been seen)
+        :WHEN:  presented for delivery and SPF still passes
+        :THEN:  it should be accepted
+        """
+        caplog.set_level(logging.DEBUG)
+        sender = passing_spf_sender
+        recip = spf_and_grl_recipients
+        message = ibm_test_message_factory(sender, recip)
+        chapps_ibm_service_with_tuples_factory([(sender, recip)])
+        with SMTP("127.0.0.1") as smtp:
+            result = smtp.sendmail(sender, recip, message)
+        assert True  # email was accepted
+
+    def test_spf_failure_enforcement(
+        self,
+        caplog,
+        clear_redis_grl,
+        chapps_ibm_service_with_tally_factory,
+        known_sender,  # fails SPF check
+        spf_and_grl_recipients,
+        ibm_test_message_factory,
+    ):
+        """
+        :GIVEN: an email fails SPF checking
+        :WHEN:  enforcing policy
+        :THEN:  it should be rejected
+        """
+        caplog.set_level(logging.DEBUG)
+        sender = known_sender
+        recip = spf_and_grl_recipients
+        message = ibm_test_message_factory(sender, recip)
+        with chapps_ibm_service_with_tally_factory([(sender, recip)]):
+            with SMTP("127.0.0.1") as smtp:
+                with pytest.raises(SMTPRecipientsRefused, match="rejected"):
+                    result = smtp.sendmail(sender, recip, message)
+
+
+class Test_IBM_No_Enforcement:
+    def test_email_is_accepted(
+        self,
+        caplog,
+        clear_redis_grl,
+        chapps_ibm_service,
+        known_sender,  # fails SPF check
+        no_enforcement_recipients,
+        ibm_test_message_factory,
+    ):
+        """
+        :GIVEN: the recipient domain enforces neither SPF or Greylisting
+        :WHEN:  an email is presented for delivery
+        :THEN:  it is accepted
+        """
+        caplog.set_level(logging.DEBUG)
+        sender = known_sender
+        recip = no_enforcement_recipients
+        message = ibm_test_message_factory(sender, recip)
+        with SMTP("127.0.0.1") as smtp:
+            result = smtp.sendmail(sender, recip, message)
         assert True
