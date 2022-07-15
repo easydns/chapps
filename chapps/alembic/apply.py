0760718523b54c886c974f005564eb7d5482a99f
Caleb Cullen <39280690+caleb-s-cullen@users.noreply.github.com>
2022-07-15 15:44:24 -0400

    provide docstring

--- chapps/alembic/apply.py
+++ chapps/alembic/apply.py
@@ -1,3 +1,4 @@
+"""Use Alembic to bring migrations up to date"""
 # Python script that will apply the migrations up to head
 # from https://stackoverflow.com/questions/42383400/python-packaging-alembic-migrations-with-setuptools
 import alembic.config
